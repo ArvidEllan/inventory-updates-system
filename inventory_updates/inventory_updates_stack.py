32610bd0b22b6d4945fcec9b4c9aad39fbc57939
Ellan G <99280261+ArvidEllan@users.noreply.github.com>
2024-04-05 09:41:16 +0300

    Update inventory_updates_stack.py on the aws services

--- inventory_updates/inventory_updates_stack.py
+++ inventory_updates/inventory_updates_stack.py
@@ -1,9 +1,27 @@
 from aws_cdk import (
-    # Duration,
+    CfnOutput,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_stepfunctions as sfn,
+    RemovalPolicy,
+    aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_s3_notifications as s3_notifications,
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_sns as sns,
+    aws_cloudwatch as cloudwatch,
+    aws_cloudwatch_actions as cloudwatch_actions,
+    aws_dynamodb as dynamodb,
+    Tags,
+    aws_logs as logs,
+    aws_pipes as pipes,
+    aws_apigateway as apigw,
+    aws_cognito as cognito
 )
 from constructs import Construct
+from aws_cdk.aws_lambda import Function, Tracing
+
 
 class InventoryUpdatesSystemStack(Stack):
 
